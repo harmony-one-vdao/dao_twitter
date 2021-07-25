02d0f4998771fb974264a536996ed9fdbf4b153a
John Ashurst <john.ashurst@intermodaltelematics.com>
2021-07-25 21:45:31 +0200

    [UPD] Readmen

--- includes/tweet_locations.py
+++ includes/tweet_locations.py
@@ -6,16 +6,20 @@ media_dir = join("tweet_data", "media")
 
 
 tweet_data = {
+    # HIP Proposals
+    join(tweets_dir, 'HIP', 'hip13.txt'): join(media_dir, 'hip', 'HIP13.png'),
 join(tweets_dir, 'HIP', 'hip8.txt'): join(media_dir, 'hip', 'HIP8.png'),
 join(tweets_dir, 'HIP', 'hip9.txt'): join(media_dir, 'hip', 'HIP9.png'),
 join(tweets_dir, 'HIP', 'hip10.txt'): join(media_dir, 'hip', 'HIP10.png'),
 join(tweets_dir, 'HIP', 'hip11.txt'): join(media_dir, 'hip', 'HIP11.png'),
 join(tweets_dir, 'HIP', 'hip12.txt'): join(media_dir, 'hip', 'HIP12.png'),
-join(tweets_dir, 'HIP', 'hip13.txt'): join(media_dir, 'hip', 'HIP13.png'),
 join(tweets_dir, 'HIP', 'hip14.txt'): join(media_dir, 'hip', 'HIP14.png'),
 join(tweets_dir, 'HIP', 'hip15.txt'): join(media_dir, 'hip', 'HIP15.png'),
+
+# generic
 join(tweets_dir, 'generic', 'dao_mission.txt'): join(media_dir, 'generic', 'Dao.png'),
 
+# HIP Voting
 }
 
 SLEEP = 24 / len(tweet_data.keys()) * 60
