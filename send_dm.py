8c05fbcae90f9895d1e9b0e38288fd926d0dc804
John Ashurst <john.ashurst@intermodaltelematics.com>
2021-11-06 23:06:52 +0100

    [UPD] Dm messages

--- send_dm.py
+++ send_dm.py
@@ -1,38 +1,93 @@
 from connection import *
 
-hip = "hip14"
+hips = ("hip14", "hip15")
 
 
 def get_message(hip: str) -> str:
     location = join(tweets_dir, "hip", f"{hip}.txt")
-    message = open_file(location)
+    message = open_file(location, remove_links=True)
+    logging.info(message)
     return message
 
 
 def get_users(hip: str) -> dict:
     dm_list = join("tweet_data", "dm_list", f"{hip}.txt")
     users = open_file(dm_list).split("\n")
-    print(users)
-    return {x: twitter_api.GetUser(screen_name=x).id for x in users}
+    rtn = {}
+    for x in users:
+        try:
+            rtn.update({x: twitter_api.GetUser(screen_name=x).id})
+        except twitter.error.TwitterError as e:
+            logging.error(f"Problem with User [ {x} ]\n{e}")
+    return rtn
 
 
 def send_direct_message(_id: int, msg: str) -> None:
-    send_msg = twitter_api.PostDirectMessage(msg, _id, return_json=True)
-    logging.info(f"Message Result:\n {send_msg}\n")
-
+    try:
+        msg = twitter_api.PostDirectMessage(msg, _id, return_json=True)
+        return msg
+    except twitter.error.TwitterError as e:
+        return {'errors': e}
+    
 
 def run(hip: str) -> None:
     msg = get_message(hip)
     users = get_users(hip)
+
+    retry = []
+    cannot_msg = []
+    not_followed_by = []
+    user_not_found = []
+
     for uname, id in users.items():
         logging.info(f"\tSending Message to:  [ {uname} ] with id [ {id} ] \n")
-        send_direct_message(id, msg)
+        result = send_direct_message(id, msg)
 
+        if not result.get("errors"):
+            logging.info(f"Message Result:  {result}\n")
+
+        elif result.get("errors"):
+            logging.error(f"Message ERROR:   {result}\n")
+
+            if (
+                result["errors"][0]["code"] == 226
+            ):  # This request looks like it might be automated.
+                retry.append(uname)
+                sleep(
+                    1200
+                )  # take a break from automation as twitter does not like it..
+
+            elif (
+                result["errors"][0]["code"] == 349
+            ):  # You cannot send messages to this user.
+                cannot_msg.append(uname)
+
+            elif (
+                result["errors"][0]["code"] == 326
+            ):  # You are sending a Direct Message to users that do not follow you.'
+                not_followed_by.append(uname)
+
+            elif result["errors"][0]["code"] == 50:  # User not found.
+                user_not_found.append(uname)
+
+            else:
+                logging.error(f"Dunno what happened, adding to failures..\n{result}\n")
+                retry.append(uname)
+
+        sleep(10)  # arbitrary time to pause between messages
+
+    save_error_or_failed_dms(hip, hip, retry)
+    save_error_or_failed_dms(hip, "cannot_msg", cannot_msg)
+    save_error_or_failed_dms(hip, "user_not_found", user_not_found)
+    save_error_or_failed_dms(hip, "notFollowed_by", not_followed_by)
+
+
+def save_error_or_failed_dms(hip: str, _type: str, data: list) -> None:
+    dm_list = join("tweet_data", "dm_list", f"{hip}-{_type}.txt")
+    with open(dm_list, "w") as f:
+        for x in data:
+            f.write(f"{x}\n")
 
 if __name__ == "__main__":
-    # twitter_user = "MaffazO"
-    # id = twitter_api.GetUser(screen_name = twitter_user).id
-    # msg = 'Vote Goddamit!!!'
-    # send_direct_message(id, msg)
-
-    run(hip)
+    for hip in hips:
+        run(hip)
--- utils/tools.py
+++ utils/tools.py
@@ -1,6 +1,18 @@
-def open_file(fn):
+def open_file(fn, remove_links=False):
     with open(fn, "r", encoding="utf8") as f:
         data = f.readlines()
         # print(data)
-        rtn = "".join(data)
+        if not remove_links:
+            rtn = "".join(data)
+        else:
+            rtn = ""
+            for x in data:
+                if x.startswith(("✍️", "Talk", "🗳️")):
+                    break
+                else:
+                    rtn += x
         return rtn
+
+
+# l = open_file(r'C:\Users\John\Documents\GIT\Harmony\Validator Dao\dao_twitter\tweet_data\text\hip\hip12.txt', remove_links=True)
+# print(l)
